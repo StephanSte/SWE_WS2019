618dfe3be232a273e9bd637e4d44efb83700208c
StephanSte <44472829+StephanSte@users.noreply.github.com>
2019-09-21 15:45:30 +0200

    plsworkyoudamnthing

---
+++ src/BIF/SWE1/UEB1.java
@@ -0,0 +1,14 @@
+package BIF.SWE1;
+
+import BIF.SWE1.interfaces.Url;
+
+public class UEB1 {
+
+	public Url getUrl(String path) {
+		Url URL = new Url(path);
+		return URL;
+	}
+
+	public void helloWorld() {
+	}
+}
